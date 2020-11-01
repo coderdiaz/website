8d39f13e893b7719b9ee50e4c4e708aab4f7776a
Javier Diaz <coderdiaz@gmail.com>
2020-11-01 13:32:10 -0600

    fix: fixed style for books section

--- src/pages/books.tsx
+++ src/pages/books.tsx
@@ -10,6 +10,7 @@ const Books = () => {
     <section className="md:pt-0 pb-2">
       <div className="container mx-auto px-6 md:px-8 lg:px-4 xl:px-0 max-w-screen-md">
         <h1 className="text-3xl md:text-5xl font-bold font-inter mb-4 dark:text-white">Books</h1>
+        <p className="leading-loose text-lg mb-6 md:mb-12 md:w-4/6 dark:text-gray-100">A working list of all of the books I'm reading in 2020. </p>
         <div className="relative mb-6 md:mb-12">
           <Image
             className="rounded-xl"
@@ -18,16 +19,16 @@ const Books = () => {
             src="/images/my-books.jpg" />
         </div>
         <p className="leading-loose text-lg mb-6 md:mb-8 dark:text-gray-100">
-          A working list of all of the books I'm reading in 2020. Ranging from algorithms to beign better developer and manager, I try to dedicate my early mornings to exploring this variety of topics.
+          Ranging from algorithms to beign better developer and manager, I try to dedicate my early mornings to exploring this variety of topics.
         </p>
         <ul className="mb-16">
           <li className="mb-4">
-            <a className="inline-flex w-full p-4 rounded-lg bg-dark-600 hover:bg-dark-700" href="https://www.smashingmagazine.com/printed-books/design-systems/">
-              <div className="relative w-38 h-38 mr-4 overflow-hidden flex-shrink-0">
+            <a className="inline-flex flex-col md:flex-row w-full p-4 rounded-lg bg-dark-600 hover:bg-dark-700" href="https://www.smashingmagazine.com/printed-books/design-systems/">
+              <div className="relative rounded-lg w-full h-48 md:w-38 md:h-38 mb-4 md:mb-0 mr-4 overflow-hidden flex-shrink-0">
                 <Image
-                  className="absolute object-cover"
-                  width={320}
-                  height={320}
+                  className="absolute object-cover rounded-lg"
+                  width={380}
+                  height={380}
                   src="/images/books/design-systems.jpg" />
               </div>
               <div className="flex flex-col">
@@ -40,12 +41,12 @@ const Books = () => {
             </a>
           </li>
           <li className="mb-4">
-            <a className="inline-flex w-full p-4 rounded-lg bg-dark-600 hover:bg-dark-700" href="https://www.amazon.com/Monolith-Microservices-Evolutionary-Patterns-Transform/dp/1492047848">
-              <div className="relative w-38 h-38 mr-4 overflow-hidden flex-shrink-0">
+            <a className="inline-flex flex-col md:flex-row w-full p-4 rounded-lg bg-dark-600 hover:bg-dark-700" href="https://www.amazon.com/Monolith-Microservices-Evolutionary-Patterns-Transform/dp/1492047848">
+              <div className="relative rounded-lg w-full h-48 md:w-38 md:h-38 mb-4 md:mb-0 mr-4 overflow-hidden flex-shrink-0">
                 <Image
-                  className="absolute object-cover"
-                  width={320}
-                  height={320}
+                  className="absolute object-cover rounded-lg"
+                  width={380}
+                  height={380}
                   src="/images/books/monolith-to-microservices.jpg" />
               </div>
               <div className="flex flex-col">
@@ -56,12 +57,12 @@ const Books = () => {
             </a>
           </li>
           <li className="mb-4">
-            <a className="inline-flex w-full p-4 rounded-lg bg-dark-600 hover:bg-dark-700" href="https://www.smashingmagazine.com/printed-books/form-design-patterns/">
-              <div className="relative w-38 h-38 mr-4 overflow-hidden flex-shrink-0">
+            <a className="inline-flex flex-col md:flex-row w-full p-4 rounded-lg bg-dark-600 hover:bg-dark-700" href="https://www.smashingmagazine.com/printed-books/form-design-patterns/">
+              <div className="relative rounded-lg w-full h-48 md:w-38 md:h-38 mb-4 md:mb-0 mr-4 overflow-hidden flex-shrink-0">
                 <Image
-                  className="absolute object-cover"
-                  width={320}
-                  height={320}
+                  className="absolute object-cover rounded-lg"
+                  width={380}
+                  height={380}
                   src="/images/books/form-design-patterns.jpg" />
               </div>
               <div className="flex flex-col">
@@ -72,12 +73,12 @@ const Books = () => {
             </a>
           </li>
           <li className="mb-4">
-            <a className="inline-flex w-full p-4 rounded-lg bg-dark-600 hover:bg-dark-700" href="https://www.amazon.com/Introducing-Go-Reliable-Scalable-Programs/dp/1491941952">
-              <div className="relative w-38 h-38 mr-4 overflow-hidden flex-shrink-0">
+            <a className="inline-flex flex-col md:flex-row w-full p-4 rounded-lg bg-dark-600 hover:bg-dark-700" href="https://www.amazon.com/Introducing-Go-Reliable-Scalable-Programs/dp/1491941952">
+              <div className="relative rounded-lg w-full h-48 md:w-38 md:h-38 mb-4 md:mb-0 mr-4 overflow-hidden flex-shrink-0">
                 <Image
-                  className="absolute object-cover"
-                  width={320}
-                  height={320}
+                  className="absolute object-cover rounded-lg"
+                  width={380}
+                  height={380}
                   src="/images/books/introducing-to-go.jpg" />
               </div>
               <div className="flex flex-col">
@@ -88,12 +89,12 @@ const Books = () => {
             </a>
           </li>
           <li className="mb-4">
-            <a className="inline-flex w-full p-4 rounded-lg bg-dark-600 hover:bg-dark-700" href="https://www.smashingmagazine.com/printed-books/smashing-book-6-new-frontiers-in-web-design/">
-              <div className="relative w-38 h-38 mr-4 overflow-hidden flex-shrink-0">
+            <a className="inline-flex flex-col md:flex-row w-full p-4 rounded-lg bg-dark-600 hover:bg-dark-700" href="https://www.smashingmagazine.com/printed-books/smashing-book-6-new-frontiers-in-web-design/">
+              <div className="relative rounded-lg w-full h-48 md:w-38 md:h-38 mb-4 md:mb-0 mr-4 overflow-hidden flex-shrink-0">
                 <Image
-                  className="absolute object-cover"
-                  width={320}
-                  height={320}
+                  className="absolute object-cover rounded-lg"
+                  width={380}
+                  height={380}
                   src="/images/books/new-frontiers-in-web-design.jpg" />
               </div>
               <div className="flex flex-col">
