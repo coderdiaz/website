a43133e9ab5ee5dc80cf9ff32f9da1a9e222f639
Javier Diaz <coderdiaz@gmail.com>
2020-10-31 18:43:12 -0600

    fix: fixed navigation

--- src/components/Navigation.tsx
+++ src/components/Navigation.tsx
@@ -10,21 +10,21 @@ const Navigation = ({ className }: INavigationProps) => {
   return <ul className={className}>
     <li className="block md:hidden mb-4 md:mb-0 md:mr-4">
       <Link href="/">
-        <a className="text-xl md:text-normal px-6 py-4 md:py-2 md:px-4 transition duration-200 ease-in-out dark:text-gray-100 dark-hover:text-gray-400 md:dark-hover:bg-dark-700 md:hover:bg-gray-50 rounded">
+        <a className="text-lg md:text-normal px-6 py-4 md:py-2 md:px-4 transition duration-200 ease-in-out dark:text-gray-100 dark-hover:text-gray-400 md:dark-hover:bg-dark-700 md:hover:bg-gray-50 rounded">
           Home
         </a>
       </Link>
     </li>
     <li className="mb-4 md:mb-0 md:mr-4">
       <Link href="/uses">
-        <a className="text-xl md:text-normal px-6 py-4 md:py-2 md:px-4 transition duration-200 ease-in-out dark:text-gray-100 md:dark-hover:bg-dark-700 md:hover:bg-gray-50 rounded">
+        <a className="text-lg md:text-normal px-6 py-4 md:py-2 md:px-4 transition duration-200 ease-in-out dark:text-gray-100 md:dark-hover:bg-dark-700 md:hover:bg-gray-50 rounded">
           Uses
         </a>
       </Link>
     </li>
     <li className="mb-4 md:mb-0 md:mr-4">
       <Link href="/about">
-        <a className="text-xl md:text-normal px-6 py-4 md:py-2 md:px-4 transition duration-200 ease-in-out dark:text-gray-100 md:dark-hover:bg-dark-700 md:hover:bg-gray-50 rounded">
+        <a className="text-lg md:text-normal px-6 py-4 md:py-2 md:px-4 transition duration-200 ease-in-out dark:text-gray-100 md:dark-hover:bg-dark-700 md:hover:bg-gray-50 rounded">
           About me
         </a>
       </Link>
