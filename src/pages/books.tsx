0b5ea7b8e641b5aa294d2c8513beefb6cbd5559b
Javier Diaz <coderdiaz@gmail.com>
2020-11-01 16:09:04 -0600

    fix: fixed colors in book items

--- src/pages/books.tsx
+++ src/pages/books.tsx
@@ -23,7 +23,7 @@ const Books = () => {
         </p>
         <ul className="mb-16">
           <li className="mb-4">
-            <a className="inline-flex flex-col md:flex-row w-full p-4 rounded-lg bg-dark-600 hover:bg-dark-700" href="https://www.smashingmagazine.com/printed-books/design-systems/">
+            <a className="inline-flex flex-col md:flex-row w-full p-4 rounded-lg bg-gray-50 hover:bg-gray-100 dark:bg-dark-600 dark-hover:bg-dark-700" href="https://www.smashingmagazine.com/printed-books/design-systems/">
               <div className="relative rounded-lg w-full h-48 md:w-38 md:h-38 mb-4 md:mb-0 mr-4 overflow-hidden flex-shrink-0">
                 <Image
                   className="absolute object-cover rounded-lg"
@@ -32,16 +32,16 @@ const Books = () => {
                   src="/images/books/design-systems.jpg" />
               </div>
               <div className="flex flex-col">
-                <span className="inline-block font-inter text-lg text-white font-semibold">
+                <span className="inline-block font-inter text-lg text-dark dark:text-white font-semibold">
                   Design Systems
                 </span>
-                <span className="inline-block text-base text-white font-semibold mb-2">by Alla Kholmatova</span>
-                <p className="leading-relaxed text-gray-400">This book presents a perspective on design systems based on the Newman's experiences as an interaction and visual designer. This is a design book, but it isn't about what to design.</p>
+                <span className="inline-block text-base dark:text-white font-semibold mb-2">by Alla Kholmatova</span>
+                <p className="leading-relaxed dark:text-gray-400">This book presents a perspective on design systems based on the Newman's experiences as an interaction and visual designer. This is a design book, but it isn't about what to design.</p>
               </div>
             </a>
           </li>
           <li className="mb-4">
-            <a className="inline-flex flex-col md:flex-row w-full p-4 rounded-lg bg-dark-600 hover:bg-dark-700" href="https://www.amazon.com/Monolith-Microservices-Evolutionary-Patterns-Transform/dp/1492047848">
+            <a className="inline-flex flex-col md:flex-row w-full p-4 rounded-lg bg-gray-50 hover:bg-gray-100 dark:bg-dark-600 dark-hover:bg-dark-700" href="https://www.amazon.com/Monolith-Microservices-Evolutionary-Patterns-Transform/dp/1492047848">
               <div className="relative rounded-lg w-full h-48 md:w-38 md:h-38 mb-4 md:mb-0 mr-4 overflow-hidden flex-shrink-0">
                 <Image
                   className="absolute object-cover rounded-lg"
@@ -50,14 +50,14 @@ const Books = () => {
                   src="/images/books/monolith-to-microservices.jpg" />
               </div>
               <div className="flex flex-col">
-                <span className="inline-block font-inter text-lg text-white font-semibold">Monolith to Microservices</span>
-                <span className="inline-block text-base text-white font-semibold mb-2">by Sam Newman</span>
-                <p className="leading-relaxed text-gray-400">This book is designed as a deep dive into how you think about, and execute, breaking apart existing systems into a microservice architecture, but the focus is on the descomposition side of things.</p>
+                <span className="inline-block font-inter text-lg text-dark dark:text-white font-semibold">Monolith to Microservices</span>
+                <span className="inline-block text-base dark:text-white font-semibold mb-2">by Sam Newman</span>
+                <p className="leading-relaxed dark:text-gray-400">This book is designed as a deep dive into how you think about, and execute, breaking apart existing systems into a microservice architecture, but the focus is on the descomposition side of things.</p>
               </div>
             </a>
           </li>
           <li className="mb-4">
-            <a className="inline-flex flex-col md:flex-row w-full p-4 rounded-lg bg-dark-600 hover:bg-dark-700" href="https://www.smashingmagazine.com/printed-books/form-design-patterns/">
+            <a className="inline-flex flex-col md:flex-row w-full p-4 rounded-lg bg-gray-50 hover:bg-gray-100 dark:bg-dark-600 dark-hover:bg-dark-700" href="https://www.smashingmagazine.com/printed-books/form-design-patterns/">
               <div className="relative rounded-lg w-full h-48 md:w-38 md:h-38 mb-4 md:mb-0 mr-4 overflow-hidden flex-shrink-0">
                 <Image
                   className="absolute object-cover rounded-lg"
@@ -66,14 +66,14 @@ const Books = () => {
                   src="/images/books/form-design-patterns.jpg" />
               </div>
               <div className="flex flex-col">
-                <span className="inline-block font-inter text-lg text-white font-semibold">Form Design Patterns</span>
-                <span className="inline-block text-base text-white font-semibold mb-2">by Adam Silver</span>
-                <p className="leading-relaxed text-gray-400">Design patterns serve as guidance and solutions to people solving similar problems over and over. The reason for design patterns is twofold. Second, by solving the same problem in the same way, users haver a consistent and more coherent experience.</p>
+                <span className="inline-block font-inter text-lg text-dark dark:text-white font-semibold">Form Design Patterns</span>
+                <span className="inline-block text-base dark:text-white font-semibold mb-2">by Adam Silver</span>
+                <p className="leading-relaxed dark:text-gray-400">Design patterns serve as guidance and solutions to people solving similar problems over and over. The reason for design patterns is twofold. Second, by solving the same problem in the same way, users haver a consistent and more coherent experience.</p>
               </div>
             </a>
           </li>
           <li className="mb-4">
-            <a className="inline-flex flex-col md:flex-row w-full p-4 rounded-lg bg-dark-600 hover:bg-dark-700" href="https://www.amazon.com/Introducing-Go-Reliable-Scalable-Programs/dp/1491941952">
+            <a className="inline-flex flex-col md:flex-row w-full p-4 rounded-lg bg-gray-50 hover:bg-gray-100 dark:bg-dark-600 dark-hover:bg-dark-700" href="https://www.amazon.com/Introducing-Go-Reliable-Scalable-Programs/dp/1491941952">
               <div className="relative rounded-lg w-full h-48 md:w-38 md:h-38 mb-4 md:mb-0 mr-4 overflow-hidden flex-shrink-0">
                 <Image
                   className="absolute object-cover rounded-lg"
@@ -82,14 +82,14 @@ const Books = () => {
                   src="/images/books/introducing-to-go.jpg" />
               </div>
               <div className="flex flex-col">
-                <span className="inline-block font-inter text-lg text-white font-semibold">Introducing to Go</span>
-                <span className="inline-block text-base text-white font-semibold mb-2">by Caleb Doxsey</span>
-                <p className="leading-relaxed text-gray-400">This book is written for relative inexperienced programmers who know nothing about Go. Although not exhaustive, it does cover all of the basics, and show leave you well positioned to tackle the more advanced material available on the language.</p>
+                <span className="inline-block font-inter text-lg text-dark dark:text-white font-semibold">Introducing to Go</span>
+                <span className="inline-block text-base dark:text-white font-semibold mb-2">by Caleb Doxsey</span>
+                <p className="leading-relaxed dark:text-gray-400">This book is written for relative inexperienced programmers who know nothing about Go. Although not exhaustive, it does cover all of the basics, and show leave you well positioned to tackle the more advanced material available on the language.</p>
               </div>
             </a>
           </li>
           <li className="mb-4">
-            <a className="inline-flex flex-col md:flex-row w-full p-4 rounded-lg bg-dark-600 hover:bg-dark-700" href="https://www.smashingmagazine.com/printed-books/smashing-book-6-new-frontiers-in-web-design/">
+            <a className="inline-flex flex-col md:flex-row w-full p-4 rounded-lg bg-gray-50 hover:bg-gray-100 dark:bg-dark-600 dark-hover:bg-dark-700" href="https://www.smashingmagazine.com/printed-books/smashing-book-6-new-frontiers-in-web-design/">
               <div className="relative rounded-lg w-full h-48 md:w-38 md:h-38 mb-4 md:mb-0 mr-4 overflow-hidden flex-shrink-0">
                 <Image
                   className="absolute object-cover rounded-lg"
@@ -98,9 +98,9 @@ const Books = () => {
                   src="/images/books/new-frontiers-in-web-design.jpg" />
               </div>
               <div className="flex flex-col">
-                <span className="inline-block font-inter text-lg text-white font-semibold">New Frontiers in Web Design</span>
-                <span className="inline-block text-base text-white font-semibold mb-2">by Smashing Magazine</span>
-                <p className="leading-relaxed text-gray-400">It’s about time to finally make sense of all the front-end and UX madness. This book contains everything from design systems to accessible single-page apps, CSS Custom Properties, CSS Grid, Service Workers, performance patterns, AR/VR, conversational UIs and responsive art direction.</p>
+                <span className="inline-block font-inter text-lg text-dark dark:text-white font-semibold">New Frontiers in Web Design</span>
+                <span className="inline-block text-base dark:text-white font-semibold mb-2">by Smashing Magazine</span>
+                <p className="leading-relaxed dark:text-gray-400">It’s about time to finally make sense of all the front-end and UX madness. This book contains everything from design systems to accessible single-page apps, CSS Custom Properties, CSS Grid, Service Workers, performance patterns, AR/VR, conversational UIs and responsive art direction.</p>
               </div>
             </a>
           </li>
