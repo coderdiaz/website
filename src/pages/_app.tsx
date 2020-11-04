50d9fc1b49bb0bcc7e900bb48a71bd52b49c75f6
Javier Diaz <coderdiaz@gmail.com>
2020-11-04 10:59:47 -0600

    feat: removed ga and improved packages

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,11 +1,9 @@
 import "@/assets/css/global.css";
 import "tailwindcss/tailwind.css";
 import type { AppProps } from "next/app";
-import Router from "next/router";
-import withGa from "next-ga";
 
 function MyApp({ Component, pageProps }: AppProps) {
   return <Component {...pageProps} />
 }
 
-export default withGa("UA-169353371-1", Router)(MyApp);
+export default MyApp;
