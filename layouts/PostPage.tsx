2dd256441c6367d77731a5abebb15a80acc78cf7
Javier <hey@coderdiaz.me>
2021-11-19 00:54:19 -0600

    fix: minor visual fixes

--- layouts/PostPage.tsx
+++ layouts/PostPage.tsx
@@ -1,10 +1,11 @@
 import Image from 'next/image'
 import { useMemo } from 'react'
 import { getMDXComponent } from 'mdx-bundler/client'
-import { ArrowLeft } from 'react-feather'
+
 import type { PageProps } from '@lib/types'
 import components from '@components/MDXComponents'
 import Container from '@components/partials/Container'
+import Avatar from '@assets/avatar.png'
 
 type Props = {} & PageProps
 
@@ -15,20 +16,19 @@ const PostPageLayout = ({ code, frontmatter }: Props) => {
     <>
       <section className="pt-8 pb-14 md:py-14">
         <Container className="max-w-3xl">
-          <a href="#" className="text-rose-500 flex space-x-3 items-center mb-6">
-            <ArrowLeft className="w-5 h-5" />
-            <span className="inline-block font-bold uppercase text-sm tracking-widest text-rose-600">Regresar</span>
-          </a>
           <article>
             <h1 className="font-bold text-3xl md:text-4xl leading-tight mb-4 md:mb-5">{frontmatter.title}</h1>
-            <div className="flex space-x-4 items-center text-gray-700 mb-4 md:mb-5">
-              <time dateTime="" className="inline-block">Noviembre 18, 2021</time>
-              <div className="w-1 h-1 rounded-full bg-gray-900" />
+            <div className="flex justify-between">
+              <div className="flex space-x-4 items-center text-gray-700 mb-6 md:mb-10">
+                <div className="flex items-center space-x-2">
+                  <Image width={24} height={24} src={Avatar} />
+                  <span className="text-gray-800 font-medium">Javier Diaz</span>
+                </div>
+                <div className="w-1 h-1 rounded-full bg-gray-900" />
+                <time dateTime="" className="inline-block">Noviembre 18, 2021</time>
+              </div>
               <span className="inline-block">{Math.ceil(frontmatter.readingTime.time/1000/60)} min. de lectura</span>
             </div>
-            <p className="leading-loose italic mb-6 md:mb-10">
-              <span className="pr-2 uppercase text-sm tracking-widest not-italic font-bold text-rose-600">Quick Summary »</span> {frontmatter.summary}
-            </p>
             <div className="relative lg:-mx-10 mb-6 md:mb-10">
               <Image
                 className="w-full h-full rounded-lg"
@@ -36,6 +36,9 @@ const PostPageLayout = ({ code, frontmatter }: Props) => {
                 height={576}
                 src={frontmatter.image} />
             </div>
+            <p className="leading-loose italic mb-6 md:mb-10">
+              <span className="pr-2 uppercase text-sm tracking-widest not-italic font-bold text-rose-600">Resumen »</span> {frontmatter.summary}
+            </p>
             <div className="prose max-w-none">
               <Component components={{
                 ...components
