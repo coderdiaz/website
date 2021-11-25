283f5941f6405f5d276ea755020dfc43dbbcf1cd
Javier <hey@coderdiaz.me>
2021-11-25 10:36:07 -0600

    feat: loaded rehype plugins

--- lib/utils.ts
+++ lib/utils.ts
@@ -1,5 +1,8 @@
 import readingTime from 'reading-time'
 import { bundleMDX } from 'mdx-bundler'
+import rehypeSlug from 'rehype-slug'
+import rehypeCodeTitles from 'rehype-code-titles'
+import rehypePrism from 'rehype-prism-plus'
 import type { BundlerResult } from '@lib/types'
 
 export async function bundler(source: string, slug: string): Promise<BundlerResult> {
@@ -8,6 +11,9 @@ export async function bundler(source: string, slug: string): Promise<BundlerResu
       options.remarkPlugins = [...(options?.remarkPlugins ?? [])];
       options.rehypePlugins = [
         ...(options?.rehypePlugins ?? []),
+        rehypeSlug,
+        rehypeCodeTitles,
+        rehypePrism,
       ]
       return options
     }
