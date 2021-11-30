f2fe5b4cd124d7291132c3ea858f4564a487d70c
Javier <hey@coderdiaz.me>
2021-11-30 15:52:12 -0600

    feat: added favicons

--- components/partials/Meta.tsx
+++ components/partials/Meta.tsx
@@ -23,8 +23,13 @@ const Meta = ({ meta }: Props) => {
   return <Head>
     <meta charSet="UTF-8" />
     <title>{meta?.title || title }</title>
-    <link rel="icon" type="image/png" sizes="32x32" href="/static/Favicon-32x32.png" />
-    <link rel="icon" type="image/png" sizes="16x16" href="/static/Favicon-16x16.png" />
+    <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png" />
+    <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" />
+    <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png" />
+    <link rel="manifest" href="/site.webmanifest" />
+    <meta name="msapplication-config" content="/browserconfig.xml" />
+    <meta name="msapplication-TileColor" content="#111827" />
+    <meta name="theme-color" content="#111827" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
     <meta httpEquiv="x-ua-compatible" content="ie=edge" />
     <meta content={meta?.description || description} name="description" />
