20bd1543b1239f55c38b6df479d10238a7f2bd39
Javier <hey@coderdiaz.me>
2021-12-04 13:16:51 -0600

    feat: added kbar to global app

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,7 +1,12 @@
 import '@styles/global.css'
+import PlausibleProvider from 'next-plausible'
 import type { NextPage } from 'next'
 import type { AppProps } from 'next/app'
-import PlausibleProvider from 'next-plausible'
+import { useRouter } from 'next/router'
+import { KBarProvider } from 'kbar'
+import { FileText, Home, Box, User, Tool, AtSign, Twitter, GitHub, Linkedin, Instagram } from 'react-feather'
+
+import KBarMenu from '@components/KBarMenu'
 
 type NextPageWithLayout = NextPage & {
   getLayout?: (page: React.ReactElement) => JSX.Element
@@ -12,12 +17,97 @@ type AppPropsWithLayout = AppProps & {
 }
 
 function MyApp({ Component, pageProps }: AppPropsWithLayout) {
+  const router = useRouter()
   const getLayout = Component.getLayout ?? ((page) => page)
 
-  return getLayout(
-    <PlausibleProvider domain="coderdiaz.me">
-      <Component {...pageProps} />
-    </PlausibleProvider>
+  // Actions
+  const initialActions = [
+    {
+      id: 'homeAction',
+      name: 'Pagina de inicio',
+      shortcut: ['h'],
+      keywords: 'back',
+      icon: <Home width={16} height={16} />,
+      perform: () => router.push('/'),
+      section: 'Navegación',
+    },
+    {
+      id: 'blogAction',
+      name: 'Blog',
+      shortcut: ['g'],
+      icon: <FileText width={16} height={16} />,
+      perform: () => router.push('/blog/'),
+      section: 'Navegación',
+    },
+    {
+      id: 'workAction',
+      name: 'Mis trabajos y proyectos',
+      shortcut: ['w'],
+      icon: <Box width={16} height={16} />,
+      perform: () => router.push('/work/'),
+      section: 'Navegación',
+    },
+    {
+      id: 'aboutAction',
+      name: 'Sobre mī',
+      shortcut: ['s'],
+      icon: <User width={16} height={16} />,
+      perform: () => router.push('/sobre-mi/'),
+      section: 'Navegación',
+    },
+    {
+      id: 'usesAction',
+      name: '¿Qué uso?',
+      shortcut: ['q'],
+      icon: <Tool width={16} height={16} />,
+      perform: () => router.push('/que-uso/'),
+      section: 'Navegación',
+    },
+    {
+      id: 'contactoAction',
+      name: 'Ponte en contacto',
+      shortcut: ['s'],
+      icon: <AtSign width={16} height={16} />,
+      perform: () => router.push('/contacto/'),
+      section: 'Navegación',
+    },
+    {
+      id: 'github',
+      name: 'Github',
+      shortcut: ['f', 'g'],
+      keywords: 'go-github',
+      section: 'Redes Sociales',
+      perform: () => (window.open('https://github.com/coderdiaz', '_blank')),
+      icon: <GitHub width={16} height={16} />,
+    },
+    {
+      id: 'twitter',
+      name: 'Twitter',
+      shortcut: ['f', 't'],
+      keywords: 'go-twitter',
+      section: 'Redes Sociales',
+      perform: () => (window.open('https://twitter.com/coderdiaz', '_blank')),
+      icon: <Twitter width={16} height={16} />,
+    },
+    {
+      id: 'linkedin',
+      name: 'LinkedIn',
+      shortcut: ['f', 'l'],
+      keywords: 'go-linkedin',
+      section: 'Redes Sociales',
+      perform: () => (window.open('https://linkedin.com/in/coderdiaz', '_blank')),
+      icon: <Linkedin width={16} height={16} />,
+    },
+  ]
+  return (
+    <>
+      <PlausibleProvider domain="coderdiaz.me">
+        <KBarProvider actions={initialActions}>
+          <KBarMenu />
+          { getLayout(<Component {...pageProps} />) }
+        </KBarProvider>
+      </PlausibleProvider>
+    </>
   )
 }
 
