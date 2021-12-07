dda7966199fcfa5dfc56af6c8d51153e3e06662e
Javier <hey@coderdiaz.me>
2021-12-07 10:24:31 -0600

    fix: fixed quality

--- layouts/PostPage.tsx
+++ layouts/PostPage.tsx
@@ -28,7 +28,14 @@ const PostPageLayout = ({ code, frontmatter }: Props) => {
             <div className="hidden md:flex justify-between">
               <div className="flex space-x-4 items-center text-gray-700 mb-6 md:mb-10">
                 <div className="flex items-center space-x-2">
-                  <Image className="rounded-full" width={24} height={24} src={Avatar} alt="Avatar de Javier Diaz" />
+                  <Image
+                    className="rounded-full"
+                    width={24}
+                    height={24}
+                    src={Avatar}
+                    quality={100}
+                    alt="Avatar de Javier Diaz"
+                    priority />
                   <span className="text-gray-800 font-medium">Javier Diaz</span>
                 </div>
                 <div className="w-1 h-1 rounded-full bg-gray-900" />
@@ -40,7 +47,14 @@ const PostPageLayout = ({ code, frontmatter }: Props) => {
             </div>
             <div className="flex md:hidden space-x-3 items-center mb-6">
               <div className="w-10 h-10 flex-shrink-0">
-                <Image width={48} height={48} src={Avatar} alt="Avatar de Javier Diaz" />
+              <Image
+                className="rounded-full"
+                width={48}
+                height={48}
+                src={Avatar}
+                quality={100}
+                alt="Avatar de Javier Diaz"
+                priority />
               </div>
               <div className="flex flex-col">
                 <span className="inline-block text-gray-800 font-medium">Javier Diaz</span>
@@ -61,6 +75,7 @@ const PostPageLayout = ({ code, frontmatter }: Props) => {
                 className="w-full h-full rounded-lg"
                 width={976}
                 height={580}
+                quality={90}
                 src={frontmatter.image} />
             </div>
             <div className="prose max-w-none">
