cf9791b086f0a7efcd9cebe41fc078228f0d47e5
Javier <hey@coderdiaz.me>
2021-12-10 15:41:00 -0600

    chore: changed from classnames to clsx

--- components/partials/Container.tsx
+++ components/partials/Container.tsx
@@ -1,11 +1,11 @@
-import cn from 'classnames'
+import clsx from 'clsx'
 import type { CommonProps } from '@lib/types'
 
 type Props = {} & CommonProps
 
 export default function Container({ className, children }: Props) {
   return (
-    <div className={cn('px-4 sm:px-6 lg:px-8 mx-auto relative z-10', className)}>
+    <div className={clsx('px-4 sm:px-6 lg:px-8 mx-auto relative z-10', className)}>
       {children}
     </div>
   );
